95335fc35ac694d9b2680a018b1bfd04d7876040
Sebastian Grabowski <sebastian@grabel.de>
2023-02-09 19:54:24 +0100

    Adapt TestSearchFilter tests to new tasks and tags

--- tests/core/test_search_filter.py
+++ tests/core/test_search_filter.py
@@ -17,9 +17,11 @@
 # -----------------------------------------------------------------------------
 
 from unittest import TestCase
+from uuid import uuid4
 
 from GTG.core.search import search_filter
 from GTG.core.dates import Date
+from GTG.core.tags2 import Tag2
 
 d = Date.parse
 
@@ -29,85 +31,86 @@ class FakeTask():
     def __init__(self, title="", body="", tags=[], due_date=""):
         self.title = title
         self.body = body
-        self.tags = tags
-        self.due_date = Date.parse(due_date)
+        self.tags = [Tag2(id=uuid4(), name=name) for name in tags]
+        self.date_due = Date.parse(due_date)
 
     def get_title(self):
         return self.title
 
-    def get_excerpt(self, strip_tags=False):
+    @property
+    def excerpt(self, strip_tags=False):
         return self.body
 
     def get_tags_name(self):
         return self.tags
 
     def get_due_date(self):
-        return self.due_date
+        return self.date_due
 
 
 class TestSearchFilter(TestCase):
 
     def test_empty(self):
-        self.assertFalse(search_filter(FakeTask()))
+        self.assertTrue(search_filter(FakeTask()))
 
     def test_single_tag(self):
-        task = FakeTask(tags=['@a'])
+        task = FakeTask(tags=['a'])
 
-        self.assertTrue(search_filter(task, {"q": [("tag", True, "@a")]}))
-        self.assertFalse(search_filter(task, {"q": [("tag", True, "@b")]}))
-        self.assertFalse(search_filter(task, {"q": [("tag", True, "@n")]}))
+        self.assertTrue(search_filter(task, {"q": [("tag", True, "a")]}))
+        self.assertFalse(search_filter(task, {"q": [("tag", True, "b")]}))
+        self.assertFalse(search_filter(task, {"q": [("tag", True, "n")]}))
 
     def test_double_tag(self):
-        p = {"q": [("tag", True, "@a"), ("tag", True, "@b")]}
+        p = {"q": [("tag", True, "a"), ("tag", True, "b")]}
 
-        self.assertTrue(search_filter(FakeTask(tags=['@a', '@b']), p))
-        self.assertTrue(search_filter(FakeTask(tags=['@b', '@a']), p))
-        self.assertTrue(search_filter(FakeTask(tags=['@b', '@a', '@a']), p))
+        self.assertTrue(search_filter(FakeTask(tags=['a', 'b']), p))
+        self.assertTrue(search_filter(FakeTask(tags=['b', 'a']), p))
+        self.assertTrue(search_filter(FakeTask(tags=['b', 'a', 'a']), p))
         self.assertTrue(search_filter(
-            FakeTask(tags=['@b', '@a', '@c', '@d']), p))
+            FakeTask(tags=['b', 'a', 'c', 'd']), p))
         self.assertTrue(search_filter(
-            FakeTask(tags=['@b', 'search', '@a']), p))
+            FakeTask(tags=['b', 'search', 'a']), p))
         self.assertTrue(search_filter(
-            FakeTask(tags=['gtg-tags-all', '@b', 'search', '@a']), p))
+            FakeTask(tags=['gtg-tags-all', 'b', 'search', 'a']), p))
         self.assertTrue(search_filter(FakeTask(tags=['gtg-tags-all',
                                                      'gtg-tags-none',
-                                                     '@b', 'search',
-                                                     '@a']), p))
+                                                     'b', 'search',
+                                                     'a']), p))
 
-        self.assertFalse(search_filter(FakeTask(tags=['@n', '@b']), p))
-        self.assertFalse(search_filter(FakeTask(tags=['@b', '@n']), p))
-        self.assertFalse(search_filter(FakeTask(tags=['@a']), p))
-        self.assertFalse(search_filter(FakeTask(tags=['@b']), p))
+        self.assertFalse(search_filter(FakeTask(tags=['n', 'b']), p))
+        self.assertFalse(search_filter(FakeTask(tags=['b', 'n']), p))
+        self.assertFalse(search_filter(FakeTask(tags=['a']), p))
+        self.assertFalse(search_filter(FakeTask(tags=['b']), p))
         self.assertFalse(search_filter(
-            FakeTask(tags=['@b', '@b', '@c', '@d']), p))
+            FakeTask(tags=['b', 'b', 'c', 'd']), p))
         self.assertFalse(search_filter(
-            FakeTask(tags=['@b', 'search', '@d']), p))
+            FakeTask(tags=['b', 'search', 'd']), p))
         self.assertFalse(search_filter(
-            FakeTask(tags=['gtg-tags-all', '@g', 'search', '@a']), p))
+            FakeTask(tags=['gtg-tags-all', 'g', 'search', 'a']), p))
         self.assertFalse(search_filter(FakeTask(tags=['gtg-tags-all',
                                                       'gtg-tags-none',
-                                                      '@@b',
-                                                      'search', '@a']), p))
+                                                      '@b',
+                                                      'search', 'a']), p))
 
     def test_simple_tag_or(self):
-        task = FakeTask(tags=['@a', '@b'])
+        task = FakeTask(tags=['a', 'b'])
 
         self.assertTrue(search_filter(
             task,
             {"q":
-                [('or', True, [("tag", True, "@a"), ("tag", True, "@b")])]}))
+                [('or', True, [("tag", True, "a"), ("tag", True, "b")])]}))
         self.assertTrue(search_filter(
             task,
             {"q":
-                [('or', True, [("tag", True, "@a"), ("tag", True, "@n")])]}))
+                [('or', True, [("tag", True, "a"), ("tag", True, "n")])]}))
         self.assertTrue(search_filter(
             task,
             {"q":
-                [('or', True, [("tag", True, "@n"), ("tag", True, "@b")])]}))
+                [('or', True, [("tag", True, "n"), ("tag", True, "b")])]}))
         self.assertFalse(search_filter(
             task,
             {"q":
-                [('or', True, [("tag", True, "@n"), ("tag", True, "@n")])]}))
+                [('or', True, [("tag", True, "n"), ("tag", True, "n")])]}))
 
     def test_simple_word_in_title(self):
         task = FakeTask(title="GTG is the best ToDo manager for GNOME")
@@ -205,3 +208,4 @@ class TestSearchFilter(TestCase):
                                       {'q': [("soon", True)]}))
         self.assertTrue(search_filter(FakeTask(due_date="someday"),
                                       {'q': [("someday", True)]}))
+
